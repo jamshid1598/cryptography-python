34449b6774dd377cc70ef6a0b2cb5307b8a1ed88
jamshid1598 <dovurovjamshid95@gmail.com>
2022-03-29 23:04:58 +0500

    encryption functionality is on progress

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.forms import Textarea
 
 # Create your models here.
+
+
+
+
+
+class CryptoKey(models.Model):
+    private_key = models.TextField()
+    public_key = models.TextField()
+    
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    
+    class Meta:
+        ordering = ['id', 'created_at']
+        verbose_name = 'RSA encryption keys'
+        verbose_name_plural = 'RSA encryption keys'
+        
+    def __str__(self):
+        return "%s"%self.id
+
+    
+class CryptoMessage(models.Model):
+    message = models.TextField()
+    encmessage = models.TextField(blank=True, null=True)
+    
+    public_key = models.TextField(blank=True, null=True)
+    
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    
+    class Meta:
+        ordering = ['id', 'created_at']
+        verbose_name = 'Message'
+        verbose_name_plural = 'Message'
+        
+    def __str__(self):
+        return "%s"%self.id
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.views.generic import (
+    CreateView, 
+    UpdateView,
+)
+
+# CRYPTOGRAPHY
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
 
 # Create your views here.
+
+from .models import (
+    CryptoKey,
+    CryptoMessage,
+)
+
+
+
+def generate_key(request, *args, **kwargs):
+    context = {}
+    
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=1024,
+        backend=default_backend()
+    )
+    public_key = private_key.public_key()
+    
+    context['private_key'] = private_key
+    context['public_key'] = public_key
+    return render(request, '', context)
